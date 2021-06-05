867e62b85733cab3ee01cb072386e54fcb8df933
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2021-06-05 20:33:06 +0300

    Delete github.com/berkcangumusisik/hrms_project/hrms directory

--- github.com/berkcangumusisik/hrms_project/hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/CityDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concretes.City;
-
-public interface CityDao extends JpaRepository<City, Integer> {
-
-}
--- github.com/berkcangumusisik/hrms_project/hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/UserDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concretes.User;
-
-public interface UserDao extends JpaRepository<User, Integer>{
-
-}
